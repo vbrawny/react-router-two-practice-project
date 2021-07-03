c4663129bde40792e19dbc109401a9afeb792ba2
venky <vbrawny@users.noreply.github.com>
2021-07-03 22:44:15 +0100

    initial commit

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Route, useParams, Link } from "react-router-dom";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+import Comments from "../components/comments/Comments";
 const DUMMY_QUOTES = [
   {
     id: "q1",
@@ -24,8 +26,24 @@ const QuoteDetail = () => {
   return (
     <section>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${Params.quoteId}`} exact>
+        <div className="centered">
+          {/* wherever it is required we will call it, but 
+         the place where is kept should fall under below tree structure
+         of the component  like
+         /quotes/${Params.quoteId}/comments
+        */}
+          <Link className="btn--flat" to={`/quotes/${Params.quoteId}/comments`}>
+            LoadComments
+          </Link>
+        </div>
+      </Route>
       <h2>QuoteDetail</h2>
       <p>{Params.quoteId}</p>
+      {/* wherever the location is we will place it */}
+      <Route path={`/quotes/${Params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </section>
   );
 };
